c3e0521a14a5676bea4f41423abd758f811e38b8
knightofnet <wolfaryx@gmail.com>
2026-04-08 15:00:27 +0200

    Ajout de la fonction erreurExit pour afficher les messages d'erreur et mise à jour des inclusions dans index.php

--- TP8/php/funcs/base_func.php
+++ TP8/php/funcs/base_func.php
@@ -2,3 +2,19 @@
 function redirectTo($url) {
     header("Location: $url");
 }
+
+/**
+Affiche un message d'erreur en rouge
+et arrêt du programme
+ **/
+function erreurExit($msg,$e=null) {
+    echo "<span class=\"rouge\">";
+    echo $msg;
+    if (isset($e)) {
+        echo " ";
+        print_r($e);
+    }
+    echo "</span><br>";
+
+    exit;
+}
